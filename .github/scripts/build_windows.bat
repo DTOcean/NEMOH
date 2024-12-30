f8ef13d93b1fac396f88a19f7bdda477b0e6c452
Mathew Topper <damm_horse@yahoo.co.uk>
2024-12-30 18:16:40 +0000

    Remove directory change from build

--- .github/scripts/build_windows.bat
+++ .github/scripts/build_windows.bat
@@ -3,7 +3,5 @@
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
-cd oneAPI-samples\DirectProgramming\Fortran\CombinationalLogic\openmp-primes
 devenv.com Nemoh.sln /build "Release|x64"
-ls
 exit /b %ERRORLEVEL%
